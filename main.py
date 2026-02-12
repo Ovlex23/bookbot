0623d9d0e25a3b5e10f85ed8e900983ed22d2983
Ovlex23 <128264080+Ovlex23@users.noreply.github.com>
2026-02-12 14:25:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("greetings boots")
+def get_book_text():
+    with open("./books/frankenstein.txt", "r") as file: # Opens file in read mode ("r"). If file doesn't exist, raises FileNotFoundError. May add >> encoding="utf-8" << if file contains non-ASCII characters. >><< added for highlighting only.
+        return file.read()
+    
+
+def main():
+    book_text = get_book_text()
+    print(book_text)  # Print book to console
+
+main()
+
+
+# ALTERNATE VERSION:
+# def main():
+#     with open("./books/frankenstein.txt", "r") as file:
+#         book_text = file.read()
+#     print(book_text)      
+
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    print(text)
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+    
+main()
