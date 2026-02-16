581905d61067f1d557347c50447e55cabfdbd2f8
Ovlex23 <128264080+Ovlex23@users.noreply.github.com>
2026-02-16 13:38:17 -0500

    finalized main.py

--- main.py
+++ main.py
@@ -1,28 +1,16 @@
-def get_book_text():
-    with open("./books/frankenstein.txt", "r") as file: # Opens file in read mode ("r"). If file doesn't exist, raises FileNotFoundError. May add >> encoding="utf-8" << if file contains non-ASCII characters. >><< added for highlighting only.
-        return file.read()
-    
-
 def main():
-    book_text = get_book_text()
-    print(book_text)  # Print book to console
-
-main()
-
-
-# ALTERNATE VERSION:
-# def main():
-#     with open("./books/frankenstein.txt", "r") as file:
-#         book_text = file.read()
-#     print(book_text)      
-
-def main():
-    book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-    print(text)
+    book = "books/frankenstein.txt"
+    book_text = get_book_text(book)
+    total_words = count_words(book_text)
+    print(f"Total words in the book: {total_words} words")
 
 def get_book_text(path):
-    with open(path) as f:
-        return f.read()
+    with open(path, "r") as file:
+        return file.read()
     
+def count_words(book_text):
+    words = book_text.split()
+    return len(words)
+
+
 main()
